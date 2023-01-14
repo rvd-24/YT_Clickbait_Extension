324b527270a9b890ed44187f8ac65bc038c9b5d0
Someshxgurram <someshgurram007@gmail.com>
2023-01-14 19:34:47 +0530

    update

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log("Extension Popup.js")
+let config = {
+    type: 'line',
+    data: data
+};
+let data = {
+  labels: [2,4,6,8,10],
+  datasets: [{
+    label: 'Clickbait Ratio',
+    data: [1,1,2,2,4],
+    fill: false, 
+    borderColor: 'black',
+    tension: 0.1
+  }]
+};
+let myChart = new Chart(document.getElementById('myChart').getContext('2d'),config);
+
